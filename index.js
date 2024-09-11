3989a5189431e2c4f3dbcfdc2ea4da112e3e563d
Évila Oliveira <evilavictoria@hotmail.com>
2024-09-10 21:42:17 -0300

    fundamentos 005

--- index.js
+++ index.js
@@ -1,25 +1,9 @@
-// arrays, objetos
-let meta = {
-    value: 'ler um livro por mês',
- //   address: 2,
-    checked: true,
- //   log: (info) => {
-   //     console.log(info)
-    }
+const start = () => {
+    let count = 0
+    while(count <= 10){
+        console.log(count)
+        count = count + 1
+    } 
+}
 
-let metas = [
-    meta, 
-    {
-        value: "caminhar 20 minutos todos os dias",
-        checked: false
-    }
-]
-
-//meta.value = "não é mais ler um livro"
-//meta.log(meta.value)
-
-console.log(metas[1].value)
-
-// function // arrow function 
-//const criarMeta = () => {}
-//function criarMeta() {}
+start()
