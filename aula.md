34aae0a5433d41de47447a61e339034346b5a3ac
Évila Oliveira <evilavictoria@hotmail.com>
2024-09-10 22:19:57 -0300

    inicia menu de aplicação

--- aula.md
+++ aula.md
@@ -58,3 +58,15 @@ Como um lego, você irá utilizar peças para criar algoritmos, ou seja, para re
 # Estrutura de repetição
 
 - [x] while
+
+# Condicionais
+- [x] switch
+
+## Módulos em Node.js:
+
+- [x] Importação de módulos (require, CommonJS)
+- [x] Biblioteca 'inquirer' para criar prompts interativos
+
+## Programação assíncrona e Promises:
+
+- [x] Uso funções assíncronas (async/await)
--- index.js
+++ index.js
@@ -1,8 +1,41 @@
-const start = () => {
-    let count = 0
-    while(count <= 10){
-        console.log(count)
-        count = count + 1
+const { select } = require('@inquirer/prompts')
+
+const start = async () => {
+    
+    while(true){
+
+        const opcao = await select({
+            message: "Menu >",
+            choices: [
+                {
+                    name: "Cadastrar meta",
+                    value: "cadastrar"
+                },
+                {
+                    name: "Listar metas",
+                    value: "listar"
+                },
+                {
+                    name: "Sair",
+                    value: "sair"
+                }
+            ]
+        })
+
+        switch(opcao) {
+            case "cadastrar":
+                console.log("vamos cadastrar")
+                break
+            case "listar":
+                console.log("vamos listar")
+                break
+            case "sair":
+                console.log("Até a próxima!")
+                return
+                    
+
+        }
+
     } 
 }
 
